55038c9058a5374fe14e3a8b8eceb708d8d158a6
winchenza-tech <winchenza@gmail.com>
2026-01-27 01:42:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -130,9 +130,9 @@ async def summarize_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # --- PROMPT İÇİNDE DEĞİŞKEN KULLANIMI ---
     # f-string sayesinde yukarıdaki FELICIA_NAME ve TUNA_NAME buraya otomatik gömülür.
     prompt = f"""
-    Aşağıdaki konuşmaları samimi, esprili, komik, muzip ve zekice laf sokmalı bir dil ile özetle . Özel kurallar:
+    Aşağıdaki konuşmaları esprili, muzip, zekice laf sokmalı ve alaycı bir dil ile özetle . Özel kurallar:
     1: Mesajlar arasında Zenithar, Gizem veya Cıtkırıldı varsa bunları özete mutlaka dahil et ama hep de onlardan bahsetme diğerleriyle eşit derecede olsun. Gizem, Cıtkırıldı ve Zenithar'a laf sokma. Bu özeti bana verdiğin saat tek sayı ise ve özette Gizem varsa ondan Kralicemiz Gizem diyerek bahset, Çift sayı ise sadece Gizem diyebilirsin.
-    2: Özette {FELICIA_NAME} ya da {TUNA_NAME} varsa onları alaya al ve ve aşağılayıp, ezikle bunun yanında hafifçe hakaret edebilirsin onlara. Eğer yoklarsa isimlerini anma.
+    2: Özette {FELICIA_NAME} ya da {TUNA_NAME} varsa onlarla dalga geç ve ve aşağılayıp, ezikle bunun yanında onlara hafifçe hakaret edebilirsin . Eğer yoklarsa isimlerini anma.
     3: Özet içerisinde asla * (yıldız) işareti kullanma.
     4: Yazılanların hepsini 'o şunu dedi bu bunu dedi' gibi aynen yazmak yerine daha çok olay olarak özetle. Daha çok ince espri kat. 
     5: İsimler çok kritiktir. Konuşma dökümünde '{FELICIA_NAME}' ve '{TUNA_NAME}' olarak geçen kişiler bellidir. Diğer benzer isimleri veya kısaltmaları (Örn: F) sakın onlarla karıştırma, ayrı kişiler olarak gör.
