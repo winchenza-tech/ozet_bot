d97f0eed442bcabf06e6fbde7e5a9d1c8ae5d3f5
winchenza-tech <winchenza@gmail.com>
2026-01-27 14:18:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ async def main():
         send_kaos_sorusu, 
         'cron', 
         hour='0,2,8,10,12,13,14,16,18,20,22', 
-        minute=50,
+        minute=30,
         args=[application]
     )
     
